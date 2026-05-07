7a8540b3064c841419c520cb8ef2efd2485ff5f8
Francisco Javier <francisco@gmail.com>
2026-05-06 18:58:53 -0600

    corrigiendo

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const multer = require("multer");
+const path = require("path");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
 app.use("/uploads", express.static("uploads"));
 
 // conexión Mongo
